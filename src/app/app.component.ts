94652d31e7c286b382367f263bd1ad7d94ca2e7f
Ryan Rasmussen <ryan.rasmussen@aviture.us.com>
2018-03-12 11:44:21 -0500

    update status bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { MenuController, Nav, Platform } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
 import { HomePage } from '../pages/home/home.page';
 import { IndividualExhibitsPage } from '../pages/individualExhibits/individual.exhibits.page';
 import { ContinuousExhibitsPage } from '../pages/continuousExhibits/continuous.exhibits.page';
@@ -20,7 +21,7 @@ export class AppComponent {
     rootPage = HomePage;
     pages: Page[];
 
-    constructor(public platform: Platform, public menu: MenuController, public splashScreen: SplashScreen) {
+    constructor(public platform: Platform, public menu: MenuController, public statusBar: StatusBar, public splashScreen: SplashScreen) {
         this.initializeApp();
 
         this.pages = [
@@ -35,6 +36,12 @@ export class AppComponent {
             // Okay, so the platform is ready and our plugins are available.
             // Here you can do any higher level native things you might need.
             this.splashScreen.hide();
+
+            if (this.platform.is('android')) {
+                this.statusBar.backgroundColorByName('black');
+            } else {
+                this.statusBar.styleDefault();
+            }
         });
     }
 
