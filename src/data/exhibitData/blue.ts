efa2194ae201af945150335b72a41002a1fe9b8e
Ryan Rasmussen <ryan.rasmussen@aviture.us.com>
2018-03-19 14:55:47 -0500

    update exhibit data

--- src/data/exhibitData/blue.ts
+++ src/data/exhibitData/blue.ts
@@ -7,6 +7,10 @@ const blueRoom: Room = {
         {
             number: '1.1.1',
             name: 'Jarvis Offutt',
+            pictures: [
+                'Offutt'
+            ],
+            audio: 'blue-artifact-5.mp3',
             description: 'Jarvis Offutt is the Namesake of Offutt Air Force Base in Bellevue, Nebraska (Formerly Fort Crook and Offutt Field.)\n\n' +
             'Offutt was born in Omaha, Nebraska on October 26, 1894, attended Omaha Central High School, and graduated from the Lawrenceville ' +
             'Preparatory School in 1913. He earned a degree from Yale University in 1917; while at Yale, he was a member of the Varsity Club, Glee ' +
@@ -24,93 +28,89 @@ const blueRoom: Room = {
             'circled the field; then-Major (United States) CharlesTinker commanded seven of the planes which flew from Fort Riley, Kansas and dropped ' +
             'a dedicatory wreath to highlight the ceremony.\n\n' +
             'On January 13, 1948 both the airfield and Fort Crook were renamed Offutt Air Force Base, ' +
-            'the home of the headquarters of Strategic Air Command.',
-            pictures: [
-                'Offutt'
-            ],
-            audio: 'blue-artifact-5.mp3'
+            'the home of the headquarters of Strategic Air Command.'
         },
         {
             number: '1.1.2',
             name: 'Jarvis Offutt Scrapbook',
-            description: 'This scrapbook is a collection of photos and documents from Jarvis Offutt’s personal life.  This includes his time at Lawrenceville' +
-            ' Preparatory School, New Jersey, from where he graduated in 1913.  The Scrapbook is open to various articles from his time at Lawrenceville ' +
-            'including a letter to his mother from Howard Roewood about Jarvis’ skills as a mandolin player.  He also attended Yale University where he ' +
-            'was a member of the Varsity Club, Glee Club, Alpha Delta Phi fraternity and a track star winning honors in the high hurdles.  His high academic ' +
-            'achievements were also recognized with his induction into the Phi Beta Kappa Society.  ',
             pictures: [
                 'MusicalClubs'
             ],
-            audio: 'blue-artifact-4.mp3'
+            audio: 'blue-artifact-4.mp3',
+            description: 'This scrapbook is a collection of photos and documents from Jarvis Offutt’s personal life.  This includes his time at Lawrenceville' +
+            ' Preparatory School, New Jersey, from where he graduated in 1913.  The Scrapbook is open to various articles from his time at Lawrenceville ' +
+            'including a letter to his mother from Howard Roewood about Jarvis’ skills as a mandolin player.  He also attended Yale University where he ' +
+            'was a member of the Varsity Club, Glee Club, Alpha Delta Phi fraternity, and was a track star winning honors in the high hurdles.  His high academic ' +
+            'achievements were also recognized with his induction into the Phi Beta Kappa Society.'
         },
         {
             number: '1.1.3',
             name: 'Martin MB-1',
-            description: 'Designed by the Glenn Martin Company, the Martin MB-1 was the first American designed heavy bomber purchased in mass and became the ' +
-            'main United States Army Air Service bomber for some time.  The bomber was powered with two Liberty 12-A engines with 400 horsepower each with a crew ' +
-            'of three to four crew members.  Weaponry consisted of 5 x 7.62mm machine guns and an offensive load of 2,000lbs of bombs.   The MB-1, despite its ' +
-            'offensive capabilities, was to serve a primary role as a reconnaissance aircraft.  It would later be replaced with an enhanced model aircraft known ' +
-            'as the MB-2.  There were other MB-1 variants tested out and worth mentioning which demonstrate what it\'s designers were envisioning for the future ' +
-            'of large aircraft.  One MB-1 was designed to carry a 37mm cannon that replaced the bow-mount machine gun position.  Another sported a third engine, ' +
-            'a Hispano-Suiza, in the nose.  The Glen Martin Passenger variant had room for ten passengers while another model, the T-1, served as mail carrier plane.',
             pictures: [
-                'DeHavilland',
+                'Martin MB-1',
             ],
-            audio: 'blue-artifact-1.mp3'
+            audio: 'blue-artifact-1.mp3',
+            description: 'Designed by the Glenn Martin Company, the Martin MB-1 was the first American designed heavy bomber purchased in mass and became the ' +
+            'main United States Army Air Service bomber for some time.  The bomber was powered with two Liberty 12-A engines with 400 horsepower each, and had a crew ' +
+            'of three to four crew members.  Weaponry consisted of 5 x 7.62mm machine guns and an offensive load of 2,000lbs of bombs.  The MB-1, despite its ' +
+            'offensive capabilities, was to serve a primary role as a reconnaissance aircraft.  It would later be replaced with an enhanced model known ' +
+            'as the MB-2.  There were other MB-1 variants which demonstrate what it\'s designers were envisioning for the future ' +
+            'of large aircraft.  One MB-1 was designed to carry a 37mm cannon that replaced the bow-mount machine gun position.  Another sported a third engine, ' +
+            'a Hispano-Suiza, in the nose.  The Glen Martin Passenger variant had room for ten passengers while another model, the T-1, served as mail carrier plane.'
         },
         {
             number: '1.1.4',
             name: 'De Havilland DH.4',
-            description: 'One of the most successful fighter aircraft of WW1, over 6,000 units were built by war’s end.  It carried a crew of two with the pilot ' +
-            'seated ahead of the gunner/observer.  There was quite a bit of separation between the two and a “speaking tube” was installed but this did little to ' +
-            'improve the already hindered communication.  The pilot had one or two forward facing machine guns, while the rear seat was fitted with two more machine ' +
-            'guns on a ring mounting.  First accepted by the US in 1917 several variants developed including reconnaissance aircraft, trainers, longe-range aircraft.  ' +
-            'The final US Army variant, the DH.4, was retired in 1932.',
             pictures: [
                 'DeHavilland',
             ],
-            audio: 'blue-artifact-1.mp3'
+            audio: 'blue-artifact-1.mp3',
+            description: 'One of the most successful fighter aircraft of WW1, over 6,000 units were built by war’s end.  It carried a crew of two with the pilot ' +
+            'seated ahead of the gunner/observer.  There was quite a bit of separation between the two and a "speaking tube" was installed, but this did little to ' +
+            'improve the already hindered communication.  The pilot had one or two forward facing machine guns, while the rear seat was fitted with two more machine ' +
+            'guns on a ring mounting.  First accepted by the US in 1917, several variants were developed including reconnaissance aircraft, trainers, and long-range aircraft.  ' +
+            'The final US Army variant, the DH.4, was retired in 1932.'
         },
         {
             number: '1.1.5',
             name: 'Vajen Bader Smoke Helmet',
-            description: 'Invented in the 1890s by Willis Vajen and William Bader, the Vajen Bader Smoke Helmet was advertised as a safeguard against smoke, gases and ' +
-            'chemicals.  The helmet is made of chamois leather and heavily padded with fleece which acts as a pressure stop against outside air trying to enter the ' +
-            'helmet.  A compact compression tank, attached to the back of the helmet, feeds air into the helmet at atmospheric pressure where the temperature is always ' +
-            '20 degrees lower than the surrounding atmosphere.  The eye and diaphragms, at the ear holes, are made of mica which allow clear eyesight and perfect ' +
-            'transmission of sound.   The air pressurization technology took Vajen and Bader ten years to perfect and made the helmet a commercial success in mining ' +
-            'as well as firefighting.  Unbeknownst to the inventors, the technology they perfected would be applied in the future to high altitude bombers during WW2.',
             pictures: [
                 'Mask'
             ],
             audio: 'blue-artifact-3.mp3',
-            background: '#f6f5f1'
+            background: '#f6f5f1',
+            description: 'Invented in the 1890\'s by Willis Vajen and William Bader, the Vajen Bader Smoke Helmet was advertised as a safeguard against smoke, gas, and ' +
+            'chemicals.  The helmet is made of chamois leather and heavily padded with fleece which acts as a pressure stop against outside air trying to enter the ' +
+            'helmet.  A compact compression tank, attached to the back of the helmet, feeds air into the helmet at atmospheric pressure where the temperature is always ' +
+            '20 degrees lower than the surrounding atmosphere.  The eye and diaphragms at the ear holes are made of mica, which allows clear eyesight and perfect ' +
+            'transmission of sound.  The air pressurization technology took Vajen and Bader ten years to perfect, and made the helmet a commercial success in mining ' +
+            'as well as firefighting.  Unbeknownst to the inventors, the technology they perfected would be applied in the future to high altitude bombers during WW2.'
         },
         {
             number: '1.1.6',
             name: 'Arisaka type 99 Bolt Action Rifle',
-            description: 'Although the type 99 didn’t appear until 1939, it is on display as a representative of the Arisaka rifles used by the growing Japanese empire ' +
-            'in the early 1930s.  The type 38 Arisaka, used during the Sino-Japanese war of the 1930s, turned out to be outdated when compared to the Chinese rifles.  ' +
-            'While the type 38 was using the 6.5mm cartridge, the Chinese were using the far superior 7.9mm which gave the Chinese an advantage at range.  The Type 99 ' +
-            'was chambered to fire a 7.7x58mm cartridge allowing it to compete with other short rifles.  It was also designed as an affordable, easier-to-produce rifle ' +
-            'which proved vital for Japan towards the end of WW2.  However, production was not a full speed at the start of the war so Japan was forced to continue use ' +
-            'of the Type 38.',
             pictures: [
                 'Rifle'
             ],
-            audio: 'blue-artifact-6.mp3'
+            audio: 'blue-artifact-6.mp3',
+            description: 'Although the type 99 didn\'t appear until 1939, it is on display as a representative of the Arisaka rifles used by the growing Japanese empire ' +
+            'in the early 1930s.  The type 38 Arisaka, used during the Sino-Japanese war of the 1930s, was outdated compared to the Chinese rifles.  ' +
+            'While the type 38 used a 6.5mm cartridge, the Chinese were using the far superior 7.9mm which gave the Chinese an advantage in range.  The Type 99 ' +
+            'was chambered to fire a 7.7x58mm cartridge allowing it to compete with other short rifles.  It was also designed as an affordable, easier-to-produce rifle ' +
+            'which proved vital for Japan towards the end of World War 2.  However, production was not at full speed at the start of the war so Japan was forced to continue use ' +
+            'of the Type 38.'
         },
         {
             number: '1.1.7',
             name: 'Japanese "Good Luck" Flag',
-            description: 'Known as yosegaki hinomaru, "good luck" flags were parting gifts for soldiers deployed overseas and carried in their pockets.  The Japanese ' +
-            'National flag, or hinomaru, was the common medium for which messages and prayers and well-fishes from family and friends.  This gathering of writings, ' +
-            'known as yosegaki, was often inscribed in a pattern radiating from the center of the flag.  Japanese soldiers carried these as a reminder of home to endure ' +
-            'difficult conditions while deployed across the Empire.',
             pictures: [
                 'Flag'
             ],
-            audio: 'blue-artifact-2.mp3'
+            audio: 'blue-artifact-2.mp3',
+            description: 'Known as yosegaki hinomaru, "good luck" flags were parting gifts for soldiers deployed overseas and carried in their pockets.  The Japanese ' +
+            'National flag, or hinomaru, was the common medium for messages, prayers, and well-wishes from family and friends.  This gathering of writings, ' +
+            'known as yosegaki, was often inscribed in a pattern radiating from the center of the flag.  Japanese soldiers carried these as a reminder of home to endure ' +
+            'difficult conditions while deployed across the Empire.'
         }
     ]
 };
