404adf04390e69bfa325aab247bf6ad2e1923bb0
Ryan Rasmussen <ryan.rasmussen@aviture.us.com>
2018-04-06 19:26:14 -0500

    new audio tour page changes

--- src/pages/audioTour/new.audio.tour.page.ts
+++ src/pages/audioTour/new.audio.tour.page.ts
@@ -1,8 +1,10 @@
 import { AlertController, NavController, Platform } from 'ionic-angular';
-import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { ChangeDetectorRef, Component, NgZone, OnDestroy } from '@angular/core';
 import { ExhibitDataService } from '../../data/exhibit.data.service';
 import { slideInOutAnimation } from './animations';
 import { Artifact, Exhibit, Room } from '../../data/model/exhibit';
+import { Media, MEDIA_ERROR, MEDIA_STATUS, MediaObject } from '@ionic-native/media';
+import { File } from '@ionic-native/file';
 
 @Component({
     selector: 'new-audio-tour-page',
@@ -25,13 +27,18 @@ export class NewAudioTourPage implements OnDestroy {
 
     slideState: string;
 
+    private _currentAudioFile: MediaObject;
+    private _status: MEDIA_STATUS;
     private _autoPlayNext: boolean = true;
 
     private _pauseSubscription: any;
     private _resumeSubscription: any;
+    private _statusSubscription: any;
+    private _successSubscription: any;
+    private _errorSubscription: any;
 
-    constructor(public exhibitDataService: ExhibitDataService, platform: Platform,
-                public alertCtrl: AlertController, public navCtrl: NavController, public cdRef: ChangeDetectorRef) {
+    constructor(public exhibitDataService: ExhibitDataService, public platform: Platform, public file: File, public media: Media,
+                public alertCtrl: AlertController, public navCtrl: NavController, public cdRef: ChangeDetectorRef, private ngZone: NgZone) {
 
         this.exhibit = exhibitDataService.getExhibitData();
 
@@ -50,6 +57,9 @@ export class NewAudioTourPage implements OnDestroy {
             this._pauseSubscription = platform.pause.subscribe(() => {
                 console.log('PLATFORM PAUSE - AUDIO TOUR PAGE');
                 this._autoPlayNext = false;
+                if (!!this._currentAudioFile && this.isPlaying()) {
+                    this._currentAudioFile.pause();
+                }
             });
 
             this._resumeSubscription = platform.resume.subscribe(() => {
@@ -62,6 +72,22 @@ export class NewAudioTourPage implements OnDestroy {
     ngOnDestroy(): void {
         this._pauseSubscription.unsubscribe();
         this._resumeSubscription.unsubscribe();
+
+        if (!!this._statusSubscription) {
+            this._statusSubscription.unsubscribe();
+        }
+
+        if (!!this._successSubscription) {
+            this._successSubscription.unsubscribe();
+        }
+
+        if (!!this._errorSubscription) {
+            this._errorSubscription.unsubscribe();
+        }
+
+        if (!!this._currentAudioFile) {
+            this._currentAudioFile.release();
+        }
     }
 
     ionViewCanLeave(): boolean {
@@ -97,6 +123,16 @@ export class NewAudioTourPage implements OnDestroy {
         this.nextClip();
     }
 
+    playPause(): void {
+        if (!!this._currentAudioFile) {
+            if (this.isPlaying()) {
+                this._currentAudioFile.pause();
+            } else {
+                this._currentAudioFile.play();
+            }
+        }
+    }
+
     hasNextClip(): boolean {
         return this.currentClipIndex < this.totalClips - 1;
     }
@@ -112,6 +148,7 @@ export class NewAudioTourPage implements OnDestroy {
         this.currentClipIndex++;
 
         this._setDisplayItemByIndex(this.currentClipIndex);
+        this._loadAudioFile();
     }
 
     previousClip(): void {
@@ -121,6 +158,11 @@ export class NewAudioTourPage implements OnDestroy {
         this.currentClipIndex--;
 
         this._setDisplayItemByIndex(this.currentClipIndex);
+        this._loadAudioFile();
+    }
+
+    isPlaying(): boolean {
+        return this._status === MEDIA_STATUS.RUNNING;
     }
 
     private _setDisplayItemByIndex(index: number): void {
@@ -166,5 +208,91 @@ export class NewAudioTourPage implements OnDestroy {
         this.currentExhibit = null;
         this.currentRoom = room;
         this.currentArtifact = artifact;
+    }
+
+    private _getAudioFileName(): string {
+        if (this.currentArtifact) {
+            return this.currentArtifact.audio;
+        } else if (this.currentRoom) {
+            return this.currentRoom.audio;
+        } else {
+            return this.exhibit.audio;
+        }
+    }
+
+    private _loadAudioFile(): void {
+
+        this.platform.ready().then(() => {
+
+            if (!!this._statusSubscription) {
+                this._statusSubscription.unsubscribe();
+                this._statusSubscription = null;
+            }
+
+            if (!!this._successSubscription) {
+                this._successSubscription.unsubscribe();
+                this._successSubscription = null;
+            }
+
+            if (!!this._errorSubscription) {
+                this._errorSubscription.unsubscribe();
+                this._errorSubscription = null;
+            }
+
+            if (!!this._currentAudioFile) {
+                this._currentAudioFile.release();
+                this._currentAudioFile = null;
+            }
+
+            let path;
+
+            if (this.platform.is('android')) {
+                path = this.file.applicationDirectory + 'www/assets/audio/' + this._getAudioFileName();
+            } else if (this.platform.is('ios')) {
+                path = 'cdvfile://localhost/bundle/www/assets/audio/' + this._getAudioFileName();
+            }
+
+            if (this.platform.is('cordova')) {
+                this._currentAudioFile = this.media.create(path);
+                this._statusSubscription = this._currentAudioFile.onStatusUpdate.subscribe((status: MEDIA_STATUS) => this._onStatusUpdate(status));
+                this._successSubscription = this._currentAudioFile.onSuccess.subscribe(() => this._onSuccess());
+                this._errorSubscription = this._currentAudioFile.onError.subscribe((error: MEDIA_ERROR) => this._onError(error));
+
+                if (this._autoPlayNext) {
+                    this._currentAudioFile.play({playAudioWhenScreenIsLocked: false});
+                }
+            }
+        });
+    }
+
+    private _onSuccess(): void {
+        this.ngZone.run(() => {
+            console.log('TRACK FINISHED');
+
+            if (this.hasNextClip() && this._autoPlayNext) {
+                this.nextClip();
+            } else {
+                const completedAlert = this.alertCtrl.create({
+                    title: 'Audio Tour Completed',
+                    subTitle: 'You have reached the end of the audio tour.',
+                    buttons: ['OK']
+                });
+
+                completedAlert.present();
+            }
+        });
+    }
+
+    private _onStatusUpdate(status: MEDIA_STATUS) {
+        this.ngZone.run(() => {
+            console.log('MEDIA STATUS', status);
+            this._status = status;
+        });
+    }
+
+    private _onError(error: MEDIA_ERROR): void {
+        this.ngZone.run(() => {
+            console.error('Media Error: ', error);
+        });
     }
 }
