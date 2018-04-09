e7e9a6f7240b1349d138f2a96095dd86239469a9
Ryan Rasmussen <ryan.rasmussen@aviture.us.com>
2018-04-09 17:31:31 -0500

    implement prev/next artifact

--- src/pages/artifact/artifact.page.ts
+++ src/pages/artifact/artifact.page.ts
@@ -15,8 +15,8 @@ export class ArtifactPage {
     currentIndex = 0;
 
     constructor(public navParams: NavParams) {
-        this.room = navParams.get('room');
-        this.artifact = navParams.get('artifact');
+        this.room = Object.create(navParams.get('room'));
+        this.artifact = Object.create(navParams.get('artifact'));
     }
 
     onSlideChanged() {
@@ -29,5 +29,31 @@ export class ArtifactPage {
 
     previousSlide() {
         this.slider.slidePrev();
+    }
+
+    hasNextArtifact(): boolean {
+        return this._getCurrentIndex() < this.room.artifacts.length - 1;
+    }
+
+    hasPreviousArtifact(): boolean {
+        return this._getCurrentIndex() > 0;
+    }
+
+    nextArtifact(): void {
+        this.artifact = this.room.artifacts[this._getCurrentIndex() + 1];
+        this.slider.slideTo(0, 0, false);
+        this.currentIndex = 0;
+    }
+
+    previousArtifact(): void {
+        this.artifact = this.room.artifacts[this._getCurrentIndex() - 1];
+        this.slider.slideTo(0, 0, false);
+        this.currentIndex = 0;
+    }
+
+    private _getCurrentIndex(): number {
+        return this.room.artifacts.findIndex((artifact) => {
+            return artifact.name === this.artifact.name;
+        });
     }
 }
